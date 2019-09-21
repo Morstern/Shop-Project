83d653bc1b40f79956e9547ecb3479f086c1c79b
Kamil Zieliński <kamilziel@tlen.pl>
2019-09-21 17:42:11 +0200

    Add build folder - without it it didn't build frontend

---
+++ frontend/build/precache-manifest.e0f01c548e2bd8fc1a98586dcb351de1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5701fb3f3ba1a87e119b4ec84c88b35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c37c30b4a51c0859ce",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "b8c37c30b4a51c0859ce",
+    "url": "/static/js/2.31cf28d1.chunk.js"
+  },
+  {
+    "revision": "6f20f5b70f92037188e3",
+    "url": "/static/js/main.b360e23d.chunk.js"
+  },
+  {
+    "revision": "88fd42f67c8f0f0217c0",
+    "url": "/static/js/runtime~main.c5192211.js"
+  }
+]);
